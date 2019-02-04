7e20f7e98dbe39eae4f818f6814525e3047687e4
Andrzej <kawus9@gmail.com>
2019-02-04 13:15:45 +0200

    added connection to mLab

--- server.js
+++ server.js
@@ -3,10 +3,11 @@ import movieRouter from './Routes/movieRoutes';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import {login, password} from "./mongoDBpass";
 
 const app = express();
 const port = process.env.PORT || 3001;
-const db = mongoose.connect('mongodb://Andrew2:iatsu666@ds233763.mlab.com:33763/ecp-api');
+const db = mongoose.connect('mongodb://<'+login+'>:<'+password+'>@ds042459.mlab.com:42459/ideas-board');
 
 app.use(cors());
 app.use(bodyParser.json());
