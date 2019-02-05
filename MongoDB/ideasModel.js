c13a9c13a915ec9bc4bfc9520a13defdbe3cd028
Andrzej <kawus9@gmail.com>
2019-02-05 17:45:11 +0200

    Changed model of data

--- MongoDB/ideasModel.js
+++ MongoDB/ideasModel.js
@@ -2,7 +2,9 @@ import mongoose from 'mongoose';
 
 const Schema = mongoose.Schema;
 const ideasModel = new Schema({
-    ideas: [{title: String, description: String, category: String}]
+    title: String, 
+    description: String, 
+    category: String
 });
 
 export default mongoose.model('ideas',ideasModel);
